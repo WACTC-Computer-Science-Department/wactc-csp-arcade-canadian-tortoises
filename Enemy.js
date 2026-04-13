66e9f09fd0556e6ff86efe82c2b88883d447cd8e
teslacoil699 <npereirareggiani2818320@woonsocketschools.com>
2026-04-13 13:02:58 -0400

    shereem edir

--- Enemy.js
+++ Enemy.js
@@ -12,6 +12,7 @@ class Enemy extends GameObject {
     super(x, y, size || 12);
     this.speed = speed || 2;
     this.health = 30;
+    this.maxHealth = this.health;
     this.damage = 10;
     this.color = "#C82626";
     this.target = null; 
@@ -34,7 +35,8 @@ class Enemy extends GameObject {
     fill(this.color);
     ellipse(this.x, this.y, this.size * 2);
     let barWidth = 50;
-    let enemyHealthPercent = this.health / 30; 
+    let enemyHealthPercent = constrain(this.health / this.maxHealth, 0, 1);
+
     fill(100);
     rect(this.x - barWidth/2, this.y - this.size - 10, barWidth, 4);
     
@@ -45,6 +47,7 @@ class Enemy extends GameObject {
   takeDamage(amount) {
     this.health -= amount;
     if (this.health <= 0) {
+      this.health = 0;
       this.alive = false;
     }
   }
@@ -57,6 +60,7 @@ class FastEnemy extends Enemy {
     super(x, y, 8, 4);  // smaller, faster
     this.color = '#ff8800';
     this.health = 15;
+    this.maxHealth = this.health;
     this.damage = 5;
   }
 }
@@ -66,6 +70,7 @@ class TankEnemy extends Enemy {
     super(x, y, 20, 1);  // bigger, slower
     this.color = '#cc0000';
     this.health = 100;
+    this.maxHealth = this.health;
     this.damage = 25;
   }
 }
@@ -75,6 +80,7 @@ class BossEnemy extends Enemy {
     super(x, y, 30, 0.5);
     this.color = '#9900ff';
     this.health = 500;
+    this.maxHealth = this.health;
     this.damage = 40;
     this.phase = 1;
   }
@@ -85,6 +91,7 @@ class miteEnemy extends Enemy {
     super(x, y, 8, 4); // smaller, faster
     this.color = "#2ba76f";
     this.health = 3;
+    this.maxHealth = this.health;
     this.damage = 1;
   }
 }
