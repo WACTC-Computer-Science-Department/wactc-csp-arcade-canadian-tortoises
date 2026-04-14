497eef2b757f4fb594ee9362bf63b86480e81edb
Emmett Kulkin <ekulkin2809002@woonsocketschools.com>
2026-04-14 13:28:37 -0400

    Update BigBullet to display fireball PNG

--- assets.js
+++ assets.js
@@ -2,10 +2,12 @@ let characterImg_down;
 let characterImg_up; 
 let characterImg_left; 
 let characterImg_right;
+let fimg; // Fire image for the big bullet
 
 function loadGameAssets(){
     characterImg_down = loadImage('assets/character_down.png');
     characterImg_up = loadImage('assets/character_up.png');
     characterImg_left = loadImage('assets/character_left.png');
     characterImg_right = loadImage('assets/character_right.png');
+    fimg = loadImage('assets/fire.png');
 }
